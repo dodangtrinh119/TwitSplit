f65965dfd4c93c9fb2825ea5ece34f9f74edbc9c
Trinh Do <dodangtrinh119@gmail.com>
2019-04-17 21:47:51 +0700

    Initial Commit

---
+++ TwitSplit-Zalora/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitSplit-Zalora
+//
+//  Created by Đăng Trình on 4/17/19.
+//  Copyright © 2019 Dang Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TwitSplit-ZaloraTests/TwitSplit_ZaloraTests.swift
@@ -0,0 +1,34 @@
+//
+//  TwitSplit_ZaloraTests.swift
+//  TwitSplit-ZaloraTests
+//
+//  Created by Đăng Trình on 4/17/19.
+//  Copyright © 2019 Dang Trinh. All rights reserved.
+//
+
+import XCTest
+@testable import TwitSplit_Zalora
+
+class TwitSplit_ZaloraTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
