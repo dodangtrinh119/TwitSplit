deb41b3e75956c4cf5071cc41330ff675b9ec615
Trinh Do <dodangtrinh119@gmail.com>
2019-04-17 23:22:11 +0700

    init project and add split alg

---
+++ TwitSplit-Zalora/Utils/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  TwitSplit-Zalora
+//
+//  Created by Đăng Trình on 4/17/19.
+//  Copyright © 2019 Dang Trinh. All rights reserved.
+//
+
+import Foundation
---
+++ TwitSplit-Zalora/Utils/TwitSplitStrategy.swift
@@ -0,0 +1,9 @@
+//
+//  TưitSplitStrategy.swift
+//  TwitSplit-Zalora
+//
+//  Created by Đăng Trình on 4/17/19.
+//  Copyright © 2019 Dang Trinh. All rights reserved.
+//
+
+import Foundation
